2bac0e4d34464b6f846594eca03c9c52370c1739
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-12-22 14:45:07 +0200

    clean train.py, used to try training different features and test them

--- src/featuresExtraction/feature_extraction.py
+++ src/featuresExtraction/feature_extraction.py
@@ -6,6 +6,15 @@ import hu_moments as hu
 from utils import gray_img, get_thresholded
 
 def feature_extraction(img):
+    """
+    Extracts a combined feature vector from the input image using HU moments and HOG features.
+
+    Parameters:
+    img (numpy.ndarray): The input image as a numpy array.
+
+    Returns:
+    numpy.ndarray: A concatenated feature vector combining HU moments and HOG features.
+    """
     img_gray = gray_img(img)
     img_bin =img_gray>125
     hu_moments_feature = hu.hu_moments(img_bin)
